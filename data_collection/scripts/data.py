12b8f3b78f6d4261b7966bf8c1804aa9818c9485
Aditya Rajguru <adityarajguru.ar@gmail.com>
2019-02-23 15:21:49 -0600

    Data collection works

--- data_collection/scripts/data.py
+++ data_collection/scripts/data.py
@@ -11,7 +11,8 @@ import numpy as np
 latest_cmd_vel = 0
 last_file_number = -1
 next_file_number = 0
-cv_bridge = CvBridge()
+bridge = CvBridge()
+last_hsv_values = np.array([0,0,0])
 image_path = '/home/nvidia/data/images/'
 vel_path = '/home/nvidia/data/cmd_vel/'
 
@@ -22,10 +23,12 @@ def image_callback(data):
     if latest_cmd_vel.linear.x==0 and latest_cmd_vel.angular.z==0:
         return
     
-    image = cv_bridge.imgmsg_to_cv2(data,desired_encoding="bgr8")
-    hsv = cv2.cvtColor(image,cv2.COLOR_BGR2HSV)
-    lower_range = np.array([3, 100, 100], dtype=np.uint8)
-    upper_range = np.array([23, 255, 255], dtype=np.uint8)
+    hsv_array = hsv_filter(data)
+    cv_image = bridge.imgmsg_to_cv2(data,desired_encoding="bgr8")   #use bgr8 instead of passthrough otherwise the orange color tape will appear to be blue.
+    blur = cv2.GaussianBlur(cv_image,(5,5),0)
+    hsv = cv2.cvtColor(blur, cv2.COLOR_BGR2HSV)
+    lower_range = np.array([hsv_array[0]-2,0,hsv_array[2]-20],dtype=np.uint8)
+    upper_range = np.array([hsv_array[0]+2,255,hsv_array[2]],dtype=np.uint8)
     mask = cv2.inRange(hsv, lower_range, upper_range)       # converted black and white image
     small_image = cv2.resize(mask,(32,32))    # resize to a 32x32 image
     if last_file_number==-1:
@@ -52,6 +55,59 @@ def image_callback(data):
 def cmd_vel_callback(data):
     global latest_cmd_vel
     latest_cmd_vel = data;
+
+
+def hsv_filter(data):
+
+    global last_hsv_values
+    original_image = bridge.imgmsg_to_cv2(data,desired_encoding="bgr8")
+    img = cv2.cvtColor(original_image, cv2.COLOR_BGR2GRAY)
+  #  img = cv2.imread('/home/aditya/catkin_ws/src/image_masking/scripts/yellow_tape_good_lighting74.png',0)
+    hsv = cv2.cvtColor(original_image,cv2.COLOR_BGR2HSV)
+    #Otsu's Binarization on Grayscale to detect X,Y positions of line.
+
+    blur = cv2.GaussianBlur(img,(5,5),0)
+
+    ret3,th3 = cv2.threshold(blur,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+    
+    im2,contours,hierarchy = cv2.findContours(th3, 1, 2)
+
+    if len(contours)>0:
+        c = max(contours,key=cv2.contourArea)
+        cnt = c
+    
+    M = cv2.moments(cnt)
+
+    cx = int(M['m10']/M['m00'])
+    cy = int(M['m01']/M['m00'])
+
+    #print("CX:"+str(cx)+" CY:" + str(cy))
+    #print("Corresponding HSV values are: "+str(hsv[cy][cx])) # x and y values are reversed in HSV MATRIX.
+    cv2.circle(img,(cx,cy), 10, (0,0,255), -1)
+
+    rect = cv2.minAreaRect(cnt)
+    box = cv2.boxPoints(rect)
+    box = np.int0(box)
+    cv2.drawContours(img,[box],0,(0,0,255),2)
+
+    rows,cols = th3.shape[:2]
+    [vx,vy,x,y] = cv2.fitLine(cnt, cv2.DIST_L2,0,0.01,0.01)
+    lefty = int((-x*vy/vx) + y)
+    righty = int(((cols-x)*vy/vx)+y)
+    #print(str((cols-1))+","+str(righty))
+    #print(str(0)+" "+str(lefty))
+    cv2.line(img,(cols-1,righty),(0,lefty),(0,255,0),2)
+    #cv2.imshow("th3",img)
+    #cv2.imshow("th32",th3)
+    #cv2.imshow("hsv",hsv)
+    #cv2.waitKey(0)
+    if(hsv[cy][cx][0]>34 or hsv[cy][cx][0]<28 or ((abs(int(hsv[cy][cx][0])-int(last_hsv_values[0])))>40) or hsv[cy][cx][2]<250  ) :          #preferably here the HSV values for the desired color would go to filter out unwanted data.
+       last_hsv_values = hsv[cy][cx]
+       return np.array([0,0,0])
+    else:
+        last_hsv_values = hsv[cy][cx]
+        return hsv[cy][cx]
+
 
 rospy.init_node('data_collection_node',anonymous=True)
 rospy.Subscriber('/camera/color/image_raw',Image,image_callback)
--- image_masking/scripts/image.py
+++ image_masking/scripts/image.py
@@ -8,25 +8,24 @@ from sensor_msgs.msg import Image
 from cv_bridge import CvBridge,CvBridgeError
 from matplotlib import pyplot as plt
 
+
 bridge = CvBridge()
 i=0
+last_hsv_values = np.array([0,0,0])
 def callback(data):
     global i
+    hsv_array = test(data)
+    print(hsv_array.shape)
     cv_image = bridge.imgmsg_to_cv2(data,desired_encoding="bgr8")   #use bgr8 instead of passthrough otherwise the orange color tape will appear to be blue.
-    #cv2.imshow('image_from_camera',cv_image)
     blur = cv2.GaussianBlur(cv_image,(5,5),0)
     hsv = cv2.cvtColor(blur, cv2.COLOR_BGR2HSV)
-    
-    np.set_printoptions(threshold=np.nan)
-    cv2.imwrite("yellow_tape_bad_lighting"+str(i)+".png",cv_image)
-    i=i+1
-    cv2.waitKey(0)
-    lower_range = np.array([25, 0, 215], dtype=np.uint8)
-    upper_range = np.array([38, 255, 255], dtype=np.uint8)
+    lower_range = np.array([hsv_array[0]-2,0,hsv_array[2]-20],dtype=np.uint8)
+    upper_range = np.array([hsv_array[0]+2,255,hsv_array[2]],dtype=np.uint8)
     mask = cv2.inRange(hsv, lower_range, upper_range)
-    res = cv2.bitwise_and(cv_image,cv_image, mask= mask) 
+    #cv2.imshow("mask",mask)
+    #cv2.waitKey(0)
     talker(mask)
-
+    
 
 def listener():
     rospy.init_node('image_listener',anonymous=True)
@@ -38,6 +37,60 @@ def talker(mask):
     pub = rospy.Publisher('masked_images',Image)
    # rospy.init_node('binary_image_publisher',anonymous=True)
     pub.publish(bridge.cv2_to_imgmsg(mask))
+
+def test(data):
+    global last_hsv_values
+    original_image = bridge.imgmsg_to_cv2(data,desired_encoding="bgr8")
+    img = cv2.cvtColor(original_image, cv2.COLOR_BGR2GRAY)
+  #  img = cv2.imread('/home/aditya/catkin_ws/src/image_masking/scripts/yellow_tape_good_lighting74.png',0)
+    hsv = cv2.cvtColor(original_image,cv2.COLOR_BGR2HSV)
+    #Otsu's Binarization on Grayscale to detect X,Y positions of line.
+
+    blur = cv2.GaussianBlur(img,(5,5),0)
+
+    ret3,th3 = cv2.threshold(blur,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+    
+    im2,contours,hierarchy = cv2.findContours(th3, 1, 2)
+
+    if len(contours)>0:
+        c = max(contours,key=cv2.contourArea)
+        cnt = c
+    
+    M = cv2.moments(cnt)
+
+    cx = int(M['m10']/M['m00'])
+    cy = int(M['m01']/M['m00'])
+
+    print("CX:"+str(cx)+" CY:" + str(cy))
+    print("Corresponding HSV values are: "+str(hsv[cy][cx])) # x and y values are reversed in HSV MATRIX.
+    cv2.circle(img,(cx,cy), 10, (0,0,255), -1)
+
+    rect = cv2.minAreaRect(cnt)
+    box = cv2.boxPoints(rect)
+    box = np.int0(box)
+    cv2.drawContours(img,[box],0,(0,0,255),2)
+
+    rows,cols = th3.shape[:2]
+    [vx,vy,x,y] = cv2.fitLine(cnt, cv2.DIST_L2,0,0.01,0.01)
+    lefty = int((-x*vy/vx) + y)
+    righty = int(((cols-x)*vy/vx)+y)
+    print(str((cols-1))+","+str(righty))
+    print(str(0)+" "+str(lefty))
+    cv2.line(img,(cols-1,righty),(0,lefty),(0,255,0),2)
+    #cv2.imshow("th3",img)
+    #cv2.imshow("th32",th3)
+    #cv2.imshow("hsv",hsv)
+    #cv2.waitKey(0)
+    if(hsv[cy][cx][0]>34 or hsv[cy][cx][0]<28 or ((abs(hsv[cy][cx][0]-last_hsv_values[0]))>40) or hsv[cy][cx][2]<250  ) :          #preferably here the HSV values for the desired color would go to filter out unwanted data.
+       last_hsv_values = hsv[cy][cx]
+       return np.array([0,0,0])
+
+    else:
+        last_hsv_values = hsv[cy][cx]
+        return hsv[cy][cx]
+    
+
+    
 
 if __name__ == '__main__':
     listener()
